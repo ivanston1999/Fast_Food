5c0ee4498b4c62b5d3bd04d1466eb56105e300a1
ivan080402 <ivansimbolon99@gmail.com>
2023-09-26 23:24:18 +0700

    Header, banner, fragment, packaging folder

--- app/src/main/java/com/android/fastfood/presentation/main/MainActivity.kt
+++ app/src/main/java/com/android/fastfood/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.fastfood
+package com.android.fastfood.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.fastfood.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
